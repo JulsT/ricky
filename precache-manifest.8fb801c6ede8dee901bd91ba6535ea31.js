101b3f4b1f9ffb580f19a9c1dd0ba45f781a3a37
JulsT <y_tarasyuk@ukr.net>
2019-04-26 22:38:26 +0300

    Updates

---
+++ precache-manifest.8fb801c6ede8dee901bd91ba6535ea31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e50d3f4af2fc171f394f",
+    "url": "/ricky/static/js/runtime~main.f15369a7.js"
+  },
+  {
+    "revision": "32966438932fa90f890e",
+    "url": "/ricky/static/js/main.33902e4e.chunk.js"
+  },
+  {
+    "revision": "0249b6d8ad4e30efa7ac",
+    "url": "/ricky/static/js/2.e7d91203.chunk.js"
+  },
+  {
+    "revision": "32966438932fa90f890e",
+    "url": "/ricky/static/css/main.2b81d529.chunk.css"
+  },
+  {
+    "revision": "839a53396d5f0fd303edac3e9361acef",
+    "url": "/ricky/index.html"
+  }
+];
